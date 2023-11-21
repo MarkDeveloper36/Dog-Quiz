ca361f5d037ded66346bf7486d78e9c9893f56aa
Mark <MarkKranenburg@hotmail.nl>
2023-11-21 17:56:52 +0100

    get img from breed to guess form api and append it to document

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const randomBreedUrl = 'https://dog.ceo/api/breeds/image/random';
 let breedToGuess;
+let breedToGuessImg;
 
-window.onload(getRandomDogBreed());
+const getBreedToGuessUrl = 'https://dog.ceo/api/breeds/image/random';
+const getImgOfByBreedUrl = 'https://dog.ceo/api/breed/hound/images';
+
+getRandomDogBreed();
 
 function getRandomDogBreed() {
-  fetch(randomBreedUrl)
+  fetch(getBreedToGuessUrl)
     .then(response => response.json())
     .then(data => {
       if (data.status === 'success') {
         showBreedToGuess(data);
+        getImgOfBreedToGuess();
       } else {
-          console.error('Er is een fout opgetreden bij het ophalen van de afbeeldingen.');
+          console.error('Er is een fout opgetreden bij het ophalen van random dog name.');
         }
       })
     .catch(error => {
@@ -23,6 +27,35 @@ function showBreedToGuess(apiData) {
   const breedData = apiData.message.match(regexToIsolateBreedName);
   breedToGuess = breedData[1];
 
-  const h1 = document.querySelector('#h1');
-  h1.innerText = 'Which one is the ' + breedToGuess + '?';
+  const breedSpan = document.querySelector('#breedSpan');
+  breedSpan.innerText = breedToGuess;
 }
+
+function getImgOfBreedToGuess() {
+  let getImgOfBreedToGuessUrl = `https://dog.ceo/api/breed/${breedToGuess}/images`;
+  
+  fetch(getImgOfBreedToGuessUrl)
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'success') {
+        showImageOfBreedToGuess(data);
+      } else {
+        console.error('something went wrong with fetching img of dog to guess.');
+      }
+    })
+    .catch(error => {
+      console.error('Somthing went wrong witch fetching data of getImgOfBreedToGuessUrl', error);
+    })
+}
+
+function showImageOfBreedToGuess(apiData) {
+  // get random img of breed to guess form api
+  highestIndexImgArrOfBreedToGuess = apiData.message.length;
+  randomIndexOfBreedToGuess = getRandomNum(0, (highestIndexImgArrOfBreedToGuess + 1));
+  // append img to the document
+  breedToGuessImg = document.createElement('img');
+  breedToGuessImg.src = apiData.message[randomIndexOfBreedToGuess];
+  document.body.appendChild(breedToGuessImg);
+}
+
+function getRandomNum(min, max) {return Math.floor(Math.random() * (max - min) + min)} // max is de exclusieve bovengrens
